8e35f8464b59a82fe1a6d4c0927a31e01eb15d9e
pedroblome <joaopedroblome78@gmail.com>
2021-10-29 02:37:18 -0300

    allow public endpoints in security

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +12,27 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
 	
+	//configura a autenticacao de acesso e login
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
+	
+	//configuracao de autorizacao
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET,"topicos").permitAll()
+		.antMatchers(HttpMethod.GET,"topicos/*").permitAll();
+	
+	}
+	
+	//configuracoes de recursos estasticos(js, png, etc)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+	}
 	
 
 }
