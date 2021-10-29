bba4da9fa8db18957839d03ab1f9c3ca3a0ef529
pedroblome <joaopedroblome78@gmail.com>
2021-10-29 03:29:12 -0300

    authenticating user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$12$cje5YMaKMpm27u.IKBACauoQmXW5njW719KkcPklfuuNFRCHNYcbm');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
